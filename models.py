Fix point_grad_to by zeroing gradients first

models.py
@@ -3,12 +3,33 @@ from torch import nn
 from torch.autograd import Variable
 
 
-class OmniglotModel(nn.Module):
+class ReptileModel(nn.Module):
+
+    def __init__(self):
+        nn.Module.__init__(self)
+
+    def point_grad_to(self, target):
+        '''
+        Set .grad attribute of each parameter to be proportional
+        to the difference between self and target
+        '''
+        is_cuda = next(self.parameters()).is_cuda
+        for p, target_p in zip(self.parameters(), target.parameters()):
+            if p.grad is None:
+                if is_cuda:
+                    p.grad = Variable(torch.zeros(p.size())).cuda()
+                else:
+                    p.grad = Variable(torch.zeros(p.size()))
+            p.grad.data.zero_()  # not sure this is required
+            p.grad.data.add_(p.data - target_p.data)
+
+
+class OmniglotModel(ReptileModel):
     """
     A model for Omniglot classification.
     """
     def __init__(self, num_classes):
-        nn.Module.__init__(self)
+        ReptileModel.__init__(self)
 
         self.num_classes = num_classes
 
@@ -57,20 +78,6 @@ class OmniglotModel(nn.Module):
         clone = OmniglotModel(self.num_classes)
         clone.load_state_dict(self.state_dict())
         return clone
-
-    def point_grad_to(self, target):
-        '''
-        Set .grad attribute of each parameter to be proportional
-        to the difference between self and target
-        '''
-        is_cuda = next(self.parameters()).is_cuda
-        for p, target_p in zip(self.parameters(), target.parameters()):
-            if p.grad is None:
-                if is_cuda:
-                    p.grad = Variable(torch.zeros(p.size())).cuda()
-                else:
-                    p.grad = Variable(torch.zeros(p.size()))
-            p.grad.data.add_(p.data - target_p.data)
 
 
 if __name__ == '__main__':

train_omniglot.py
@@ -41,6 +41,8 @@ parser.add_argument('--shots', default=1, type=int, help='shots per class (K-sho
 parser.add_argument('--meta-iterations', default=400000, type=int, help='number of meta iterations')
 parser.add_argument('--iterations', default=3, type=int, help='number of base iterations')
 parser.add_argument('--batch', default=8, type=int, help='minibatch size in base task')
+parser.add_argument('--meta-lr', default=1e-3, type=float, help='meta learning rate')
+parser.add_argument('--lr', default=1e-2, type=float, help='base learning rate')
 
 # - General params
 parser.add_argument('--validation', default=0.1, type=float, help='Percentage of validation')
@@ -119,13 +121,14 @@ def do_evaluation(net, test_iter, iterations):
 
 
 # Main loop
-meta_optimizer = torch.optim.Adam(meta_net.parameters())
+meta_optimizer = torch.optim.Adam(meta_net.parameters(), lr=args.meta_lr)
 info = {}
 for meta_iteration in tqdm(xrange(args.meta_iterations)):
 
     # Clone model
     net = meta_net.clone()
-    optimizer = torch.optim.Adam(net.parameters())
+    #optimizer = torch.optim.Adam(net.parameters(), lr=args.lr)
+    optimizer = torch.optim.SGD(net.parameters(), lr=args.lr)
     # load state of base optimizer?
 
     # Sample base task from Meta-Train