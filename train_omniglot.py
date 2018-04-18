399d228fc137304edeed611410d2b19084a09978
Gabriel Huang <gbxhuang@gmail.com>
2018-04-18 19:12:50 -0400

    hyperparameters

--- train_omniglot.py
+++ train_omniglot.py
@@ -38,11 +38,13 @@ parser = argparse.ArgumentParser('Train reptile on omniglot')
 # - Training params
 parser.add_argument('--classes', default=5, type=int, help='classes in base-task (N-way)')
 parser.add_argument('--shots', default=1, type=int, help='shots per class (K-shot)')
-parser.add_argument('--meta-iterations', default=400000, type=int, help='number of meta iterations')
+parser.add_argument('--train-shots', default=15, type=int, help='train shots')
+parser.add_argument('--meta-iterations', default=100000, type=int, help='number of meta iterations')
 parser.add_argument('--iterations', default=3, type=int, help='number of base iterations')
-parser.add_argument('--batch', default=8, type=int, help='minibatch size in base task')
-parser.add_argument('--meta-lr', default=1e-3, type=float, help='meta learning rate')
-parser.add_argument('--lr', default=1e-1, type=float, help='base learning rate')
+parser.add_argument('--test-iterations', default=5, type=int, help='number of base iterations')
+parser.add_argument('--batch', default=25, type=int, help='minibatch size in base task')
+parser.add_argument('--meta-lr', default=0.1, type=float, help='meta learning rate')
+parser.add_argument('--lr', default=1e-3, type=float, help='base learning rate')
 
 # - General params
 parser.add_argument('--validation', default=0.1, type=float, help='Percentage of validation')
@@ -51,6 +53,8 @@ parser.add_argument('--input', default='omniglot', help='Path to omniglot datase
 parser.add_argument('--output', help='Where to save models')
 parser.add_argument('--cuda', default=1, type=int, help='Use cuda')
 args = parser.parse_args()
+if args.train_shots <= 0:
+    args.train_shots = args.shots
 
 # Load data
 # Resize is done by the MetaDataset because the result can be easily cached
@@ -112,7 +116,7 @@ def do_evaluation(net, test_iter, iterations):
 
         # Get accuracy
         argmax = net.predict(prediction)
-        accuracy = (argmax==labels).float().mean()
+        accuracy = (argmax == labels).float().mean()
 
         losses.append(loss.data[0])
         accuracies.append(accuracy.data[0])
@@ -120,18 +124,24 @@ def do_evaluation(net, test_iter, iterations):
     return np.mean(losses), np.mean(accuracies)
 
 
+def get_optimizer(net):
+    optimizer = torch.optim.Adam(net.parameters(), lr=args.lr, betas=(0, 0.999))
+    return optimizer
+
+
 # Main loop
-meta_optimizer = torch.optim.Adam(meta_net.parameters(), lr=args.meta_lr)
+meta_optimizer = torch.optim.SGD(meta_net.parameters(), lr=args.meta_lr)
+#meta_optimizer = torch.optim.Adam(meta_net.parameters(), lr=args.meta_lr)
 info = {}
 for meta_iteration in tqdm(xrange(args.meta_iterations)):
 
     # Clone model
     net = meta_net.clone()
-    optimizer = torch.optim.SGD(net.parameters(), lr=args.lr)
+    optimizer = get_optimizer(net)
     # load state of base optimizer?
 
     # Sample base task from Meta-Train
-    train = meta_train.get_random_task(args.classes, args.shots)
+    train = meta_train.get_random_task(args.classes, args.train_shots)
     train_iter = make_infinite(DataLoader(train, args.batch, shuffle=True))
 
     # Update fast net
@@ -143,19 +153,19 @@ for meta_iteration in tqdm(xrange(args.meta_iterations)):
 
     # Meta-Evaluation
     if meta_iteration % args.validate_every == 0:
-        for (meta_dataset, mode) in [(meta_train, 'train'), (meta_test, 'val')]:
+        for (meta_dataset, mode) in [(meta_train, 'train'), (meta_test, 'test')]:
 
-            train, test = meta_dataset.get_random_task_split(args.classes, train_K=args.shots, test_K=1)
+            train, test = meta_dataset.get_random_task_split(args.classes, train_K=args.shots, test_K=5)  # is that 5 ok?
             train_iter = make_infinite(DataLoader(train, args.batch, shuffle=True))
             test_iter = make_infinite(DataLoader(test, args.batch, shuffle=True))
 
             # Base-train
             net = meta_net.clone()
-            optimizer = torch.optim.SGD(net.parameters(), lr=args.lr)
-            loss = do_learning(net, optimizer, train_iter, args.iterations)
+            optimizer = get_optimizer(net)
+            loss = do_learning(net, optimizer, train_iter, args.test_iterations)
 
             # Base-test: compute meta-loss, which is base-validation error
-            meta_loss, meta_accuracy = do_evaluation(net, test_iter, args.iterations)
+            meta_loss, meta_accuracy = do_evaluation(net, test_iter, 1)  # only one iteration for eval
 
             info.setdefault(mode, {})
             info[mode].setdefault('loss', [])
