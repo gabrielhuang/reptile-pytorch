142f8cd35cb371ddb6f1f19a30b9c508b1040017
Gabriel Huang <gbxhuang@gmail.com>
2018-04-19 00:45:34 -0400

    add compatibility

--- omniglot.py
+++ omniglot.py
@@ -2,12 +2,56 @@ from torch.utils import data
 import os
 import numpy as np
 from PIL import Image
-from torchvision.datasets.utils import list_dir, list_files
 from torchvision import transforms
 
 # Might need to manually download, extract, and merge
 # https://github.com/brendenlake/omniglot/blob/master/python/images_background.zip
 # https://github.com/brendenlake/omniglot/blob/master/python/images_evaluation.zip
+
+
+# Those two functions are taken from torchvision code because they are not available on pip as of 0.2.0
+def list_dir(root, prefix=False):
+    """List all directories at a given root
+    Args:
+        root (str): Path to directory whose folders need to be listed
+        prefix (bool, optional): If true, prepends the path to each result, otherwise
+            only returns the name of the directories found
+    """
+    root = os.path.expanduser(root)
+    directories = list(
+        filter(
+            lambda p: os.path.isdir(os.path.join(root, p)),
+            os.listdir(root)
+        )
+    )
+
+    if prefix is True:
+        directories = [os.path.join(root, d) for d in directories]
+
+    return directories
+
+
+def list_files(root, suffix, prefix=False):
+    """List all files ending with a suffix at a given root
+    Args:
+        root (str): Path to directory whose folders need to be listed
+        suffix (str or tuple): Suffix of the files to match, e.g. '.png' or ('.jpg', '.png').
+            It uses the Python "str.endswith" method and is passed directly
+        prefix (bool, optional): If true, prepends the path to each result, otherwise
+            only returns the name of the files found
+    """
+    root = os.path.expanduser(root)
+    files = list(
+        filter(
+            lambda p: os.path.isfile(os.path.join(root, p)) and p.endswith(suffix),
+            os.listdir(root)
+        )
+    )
+
+    if prefix is True:
+        files = [os.path.join(root, d) for d in files]
+
+    return files
 
 
 def read_image(path, size=None):
